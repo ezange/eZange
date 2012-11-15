5f037e0b10daf26fe739281aa43d1067b83fa288
CCzer0 <xiexiaoxiexiao2009@gmail.com>
2012-11-15 15:43:12 -0500

    based on new erd- edited by xiao Nov15th

---
+++ ezange/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ezange::Application.config.secret_token = 'e2dba88a6420f97adb553039b585565d1572bfd9ad34ff5939a28aac326ce2940b55622dd24fffac109fa7413bff2effc8ea67eb9aaa06f657bce4e4a8983954'
